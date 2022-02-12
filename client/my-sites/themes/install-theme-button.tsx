a7c8cb3a6908fda5a4aa860b61a0da69fc21d8fa
Jeremy Yip <jeremy.yip@automattic.com>
2022-02-12 14:03:21 -0800

    Convert install theme button to typescript

--- client/my-sites/themes/install-theme-button.tsx
+++ client/my-sites/themes/install-theme-button.tsx
@@ -1,17 +1,18 @@
 import { Button } from '@automattic/components';
 import { translate } from 'i18n-calypso';
 import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import { isJetpackSiteMultiSite, isJetpackSite } from 'calypso/state/sites/selectors';
 import siteCanUploadThemesOrPlugins from 'calypso/state/sites/selectors/can-upload-themes-or-plugins';
+import { IAppState } from 'calypso/state/types';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import { trackClick } from './helpers';
-
 import './install-theme-button.scss';
 
-function getInstallThemeSlug( siteSlug, canUploadThemesOrPlugins ) {
+function getInstallThemeSlug( siteSlug: string | null, canUploadThemesOrPlugins: boolean ) {
 	if ( ! siteSlug ) {
 		return '/themes/upload';
 	}
@@ -23,6 +24,20 @@ function getInstallThemeSlug( siteSlug, canUploadThemesOrPlugins ) {
 	return `/themes/upload/${ siteSlug }`;
 }
 
+interface TracksEventProps {
+	tracksEventProps: { site_type: string | null };
+}
+
+interface InstallThemeButton {
+	isMultisite: boolean | null;
+	jetpackSite: boolean | null;
+	isLoggedIn: boolean;
+	siteSlug: string | null;
+	dispatchTracksEvent( { tracksEventProps }: TracksEventProps ): void;
+	canUploadThemesOrPlugins: boolean;
+	atomicSite: boolean | null;
+}
+
 const InstallThemeButton = ( {
 	isMultisite,
 	jetpackSite,
@@ -31,12 +46,12 @@ const InstallThemeButton = ( {
 	dispatchTracksEvent,
 	canUploadThemesOrPlugins,
 	atomicSite,
-} ) => {
+}: InstallThemeButton ) => {
 	if ( ! isLoggedIn || isMultisite ) {
 		return null;
 	}
 
-	let siteType = null;
+	let siteType: string | null = null;
 	if ( ! isLoggedIn ) {
 		siteType = 'logged_out';
 	} else if ( atomicSite ) {
@@ -67,7 +82,7 @@ const InstallThemeButton = ( {
 	);
 };
 
-const mapStateToProps = ( state ) => {
+const mapStateToProps = ( state: IAppState ) => {
 	const selectedSiteId = getSelectedSiteId( state );
 	return {
 		siteSlug: getSelectedSiteSlug( state ),
@@ -79,8 +94,8 @@ const mapStateToProps = ( state ) => {
 	};
 };
 
-const mapDispatchToProps = ( dispatch ) => ( {
-	dispatchTracksEvent: ( { tracksEventProps } ) =>
+const mapDispatchToProps = ( dispatch: Dispatch ) => ( {
+	dispatchTracksEvent: ( { tracksEventProps }: TracksEventProps ) =>
 		dispatch( recordTracksEvent( 'calypso_click_theme_upload', tracksEventProps ) ),
 } );
 
