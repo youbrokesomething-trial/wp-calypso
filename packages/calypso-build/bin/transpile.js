b7ba4f23193fedc589539aee2d59fd012cf551ae
Noah Allen <noahtallen@gmail.com>
2022-03-16 18:51:48 -0700

    Update transpilation script to search monorepo for babel

--- packages/calypso-build/bin/transpile.js
+++ packages/calypso-build/bin/transpile.js
@@ -32,18 +32,40 @@ for ( const arg of process.argv.slice( 2 ) ) {
 // root directory (set as cwd) which isn't an ancestor of any of the source files.
 const testIgnorePattern = path.join( dir, '**/test/**' );
 
-console.log( 'Building %s', dir );
+// Try a couple of levels up as well to get it working in a monorepo where deps are hoisted.
+const possibleRoots = [ root, path.dirname( root ), path.dirname( path.dirname( root ) ) ];
+let thisRootActuallyWorks = root;
+for ( const testRoot of possibleRoots ) {
+	thisRootActuallyWorks = testRoot;
+	try {
+		// The -f flag essentially gets babel to exit with a 0 exit code. Otherwise,
+		// it fails because the required arguments don't exist. This makes it easier
+		// to test that the binary at least exists.
+		execSync( 'npx --no-install babel -f this-is-fake-and-doesnt-exist', {
+			cwd: testRoot,
+			encoding: 'utf8',
+			stdio: 'pipe', // Stops it from printing "not found" to the console.
+		} );
+	} catch ( e ) {
+		if ( e.stderr?.includes( 'not found: babel' ) ) {
+			continue; // Try the next possible root.
+		}
+	}
+	// If we made it here, it appears that the babel command at least exits.
+	break;
+}
+
 const baseCommand = `npx --no-install babel --presets="${ babelPresetFile }" --ignore "${ testIgnorePattern }" --extensions='.js,.jsx,.ts,.tsx'`;
 
 if ( transpileAll || transpileESM ) {
 	execSync( `${ baseCommand } -d "${ outputDirESM }" "${ inputDir }"`, {
-		cwd: root,
+		cwd: thisRootActuallyWorks,
 	} );
 }
 
 if ( transpileAll || transpileCJS ) {
 	execSync( `${ baseCommand } -d "${ outputDirCJS }" "${ inputDir }"`, {
 		env: Object.assign( {}, process.env, { MODULES: 'commonjs' } ),
-		cwd: root,
+		cwd: thisRootActuallyWorks,
 	} );
 }
